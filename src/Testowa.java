ca27391d763d4275711efaf39c88ff19a4544c8a
tomasz <tomasz.z.seidler@gmail.com>
2017-08-21 08:47:19 +0200

    v2

--- src/Testowa.java
+++ src/Testowa.java
@@ -1,5 +1,17 @@
+import java.io.FileNotFoundException;
+import java.util.Formatter;
+
 public class Testowa {
     public static void main(String[] args) {
         System.out.println("test!");
+        System.out.println("test2!");
+        try {
+            Formatter f = new Formatter("test.dat");
+            f.format("%s", "abc\n");
+            f.format("%s", "abc\n");
+            f.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
     }
 }
